37582db40963097533085c5b8821ffe6100962da
Kent <kenttdang@gmail.com>
2025-02-02 23:21:02 -0500

    Added task

---
+++ TODO.md
@@ -0,0 +1,10 @@
+TODO:
+- [ ] OpenAI vs. DeepSeek
+    - OpenAI provides more consistent but worse results
+    - DeepSeek and Qwen provide better results but tend to provide duplicate entities/relationships/tripletes and hallucinates more
+    - Ideally, we would want to access the OpenAI reasoning models or create our own reasoning flow
+    - BONUS: Train/Fine-Tune our own model 
+- [ ] Prompt Engineering: Specify Defintions of each collection, entity, and gear the examples to the IoT space
+- [ ] Post-Processing Data: How can we ensure we don't add duplicates into the database, remove IDs with special characters, and ensure all relationships/collections exist
+- [ ] Proxy Rotation: We are getting blocked for 2/5 sites we scrape
+Finished:
--- server/scripts/analysis/generate.py
+++ server/scripts/analysis/generate.py
@@ -7,50 +7,51 @@ def generate(entities):
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
-            {"role": "system", "content": """
-    You are a data engineer specialized in constructing knowledge graphs. You are given a content from text, images, video, and code. Generate a set of triplets in the format ((type1, entity1), relationship, (type2, entity2)) by following these steps:
-    1. determine the type of each entity. Entities are limited to the following: device, manufacturer, application, process, sensor, 
-        observation, inference, research, privacyPolicy, regulation. DO NOT PRINT A RESPONSE FOR THIS STEP
-    2. determine the types of relationships occuring between each entity. Relationships are limited to the following: developedBy, 
-        manufacturedBy, compatibleWith, hasSensor, accessSensor, requiresSensor, performs, hasPolicy, statesInPolicy, captures, canInfer, 
-        showsInference, hasTopic, follows. DO NOT PRINT A RESPONSE FOR THIS STEP
-    3. Form a set of triplets in the format (('type1', 'entity1'), 'relationship', ('type2', 'entity2')) by following the entity-relationship-entity
-        schema:
-    application -> developedBy -> manufacturer
-    device -> manufacturedBy -> manufacturer
-    sensor -> manufacturedBy -> manufactuerer
-    device -> compatibleWith -> application
-    device -> compatibleWith -> device
-    device -> hasSensor -> sensor
-    application -> accessSensor -> sensor
-    process -> requiresSensor -> sensor
-    device -> performs -> process
-    application -> performs -> process
-    device -> hasPolicy -> privacyPolicy
-    application -> hasPolicy -> privacyPolicy
-    manufacturer -> hasPolicy -> privacyPolicy
-    process -> statesInPolicy -> privacyPolicy
-    sensor -> statesInPolicy -> privacyPolicy
-    observation -> statesInPolicy -> privacyPolicy
-    sensor -> captures -> observation
-    observation -> canInfer -> inference
-    inference -> canInfer -> inference
-    inference -> showsReference -> research
-    research -> references -> research
-    research -> hasTopic -> process
-    research -> hasTopic -> application
-    research -> hasTopic -> observation
-    research -> hasTopic -> sensor
-    research -> hasTopic -> device
-    privacyPolicy -> follows -> regulation
+            {"role": "system", "content": 
+            """
+                You are a data engineer specialized in constructing knowledge graphs. You are given a content from text, images, video, and code. Generate a set of triplets in the format ((type1, entity1), relationship, (type2, entity2)) by following these steps:
+                1. determine the type of each entity. Entities are limited to the following: device, manufacturer, application, process, sensor, 
+                    observation, inference, research, privacyPolicy, regulation. DO NOT PRINT A RESPONSE FOR THIS STEP
+                2. determine the types of relationships occuring between each entity. Relationships are limited to the following: developedBy, 
+                    manufacturedBy, compatibleWith, hasSensor, accessSensor, requiresSensor, performs, hasPolicy, statesInPolicy, captures, canInfer, 
+                    showsInference, hasTopic, follows. DO NOT PRINT A RESPONSE FOR THIS STEP
+                3. Form a set of triplets in the format (('type1', 'entity1'), 'relationship', ('type2', 'entity2')) by following the entity-relationship-entity
+                    schema:
+                application -> developedBy -> manufacturer
+                device -> manufacturedBy -> manufacturer
+                sensor -> manufacturedBy -> manufactuerer
+                device -> compatibleWith -> application
+                device -> compatibleWith -> device
+                device -> hasSensor -> sensor
+                application -> accessSensor -> sensor
+                process -> requiresSensor -> sensor
+                device -> performs -> process
+                application -> performs -> process
+                device -> hasPolicy -> privacyPolicy
+                application -> hasPolicy -> privacyPolicy
+                manufacturer -> hasPolicy -> privacyPolicy
+                process -> statesInPolicy -> privacyPolicy
+                sensor -> statesInPolicy -> privacyPolicy
+                observation -> statesInPolicy -> privacyPolicy
+                sensor -> captures -> observation
+                observation -> canInfer -> inference
+                inference -> canInfer -> inference
+                inference -> showsReference -> research
+                research -> references -> research
+                research -> hasTopic -> process
+                research -> hasTopic -> application
+                research -> hasTopic -> observation
+                research -> hasTopic -> sensor
+                research -> hasTopic -> device
+                privacyPolicy -> follows -> regulation
 
-    Do not include the word json in front of your result.
-    If no triplets are found, return an empty list ([]).
+                Do not include the word json in front of your result.
+                If no triplets are found, return an empty list ([]).
 
-    Output: a set of triplets (('type1', 'name1'), relationship, ('type2', 'name2')) in a list or an empty list.
-    
-    DO NOT RETURN THE WORD 'JSON' in the beginning of your response.
-                                        """
+                Output: a set of triplets (('type1', 'name1'), relationship, ('type2', 'name2')) in a list or an empty list.
+                
+                DO NOT RETURN THE WORD 'JSON' in the beginning of your response.
+            """
             },
             {"role": "user", "content": entities}
         ]
