466033d902f5e50af1734d18eb856c7919b81219
Gia Oriana Santos <gsantos2@ada.cm.cluster>
2025-04-02 17:49:01 -0400

    adding html extracting script

---
+++ server/scripts/extract_htmls.py
@@ -0,0 +1,86 @@
+from tqdm import tqdm
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+import requests
+from queue import Queue
+import threading
+from scraping import download_proxy, test_proxy, load_proxies, find_working_proxy, local_access
+import logging
+
+logging.basicConfig(
+    filename='html_extractor.log',  # Log file name
+    filemode = "a",
+    level=logging.INFO,  # Log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+)
+
+URLS_FILE = "Amazon_product_urls.txt"
+FOLDER_NAME = "amazon_htmls"
+GROUP = "amazon"
+
+working_proxy = None
+thread_lock = threading.Lock()
+PROXIES = []
+fake_useragent = UserAgent()
+failed_proxies = set()
+
+def extract_html(url, html_number):
+    file_name = f"{GROUP}{html_number}.html"
+    global working_proxy
+    print(f"[🔍] Scraping url:", url)
+    
+    if local_access(url):
+        print("[✅] Local access successful.")
+    else:
+        download_proxy()
+        load_proxies()
+        working_proxy = find_working_proxy()
+        if working_proxy is None:
+            load_proxies()
+            working_proxy = find_working_proxy()
+        if working_proxy is None:
+            print("[❌] No working proxy found.")
+            return None
+        
+        print(f"[🛰️] Using working proxy: {working_proxy}")
+
+    return
+
+    # Selenium WebDriver Configuration
+    options = Options()
+    options.headless = True
+    fake_useragent = UserAgent()
+    options.add_argument(f'user-agent={fake_useragent.random}')
+    
+    if working_proxy:
+        options.add_argument(f'--proxy-server={working_proxy}')
+    
+    options.add_argument("--headless")  # Run headless
+    options.add_argument("--no-sandbox")  # Necessary for some restricted environments
+    options.add_argument("--disable-dev-shm-usage")  # Overcome resource limitations
+        
+    driver = webdriver.Chrome(options=options)
+    try:
+        driver.get(url)
+        html = driver.page_source
+
+        file_path = os.path.join(FOLDER_NAME, file_name)
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(html)
+    except Exception as e:
+        print(f"Page crash detected for {file_name}, skipping...")
+        print(f"Error: {e}")  # Print the specific error
+        logging.error(f"Error extracting contents from {GROUP} link {html_number}")
+            
+    
+
+if __name__ == '__main__':
+    urls = []
+    # gets the product urls from the txt file
+    with open(URLS_FILE, "r") as f:
+        urls = [url.strip() for url in f.readlines()]
+
+    for i in tqdm(range(len(urls))):
+        extract_html(urls[i], i)
+        
