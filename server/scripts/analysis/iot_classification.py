d4aec78cc4ddb26f4946ef826492163291f49c68
Gia Oriana Santos <gsantos2@chip.cm.cluster>
2025-05-04 10:38:39 -0400

    added script to test if device is IoT or not

--- server/scripts/analysis/iot_classification.py
+++ server/scripts/analysis/iot_classification.py
@@ -2,113 +2,122 @@ import re
 import ollama
 import torch
 from util.llm_utils.response_cleaner import extract_json, extract_python, remove_think_tags
+import ast
+import random
+import os
+import argparse
+from tqdm import tqdm
 
-def generate(entities):
+FOLDER = "analysis_output"
+RETRIES = 3
+
+def product_classify(text_content):
     """
     generates triplets given a json of entities
     Input: entities ({"entities": [...]})
     Output: list of triplets ([...])
     """
+
+    #text_dict = ast.literal_eval(text_content)
+
+    product_name = text_content.get("product_name")
+
+    if not product_name:
+        return False
     
     # initializes ollama client
     client = ollama.Client()
+
+    for i in range(RETRIES):
     
-    # empties the cache
-    torch.cuda.empty_cache()
-
-    # prompts deepseek to make triplets given a json on entities
-    response = client.chat(
-        model='deepseek-r1:70b', 
-        messages=[
-            {
-                'role': 'system',
-                'content':
-                """
-                    You are a data engineer specialized in constructing knowledge graphs. Given a set of extracted entities, generate triplets in the format:
-
-                ```
-                [(('type1', 'entity1'), 'relationship', ('type2', 'entity2')), ...]
-                ```
-
-                ### **Strict Rules:**
-                - **Output only the list of triplets.**  
-                - **No explanations, bullet points, summaries, or additional text.**  
-                - **Do not introduce or describe the output.**  
-                - **If no valid triplets exist, return `[]` exactly.**
-
-                ### **Entity Types:**
-                - device, manufacturer, application, process, sensor, observation, inference, research, privacyPolicy, regulation  
-
-                ### **Relationships:**
-                - developedBy, manufacturedBy, compatibleWith, hasSensor, accessSensor, requiresSensor, performs, hasPolicy, statesInPolicy, captures, canInfer, showsInference, hasTopic, follows  
-
-                ### **Triplet Schema:**
-                ```
-                application -> developedBy -> manufacturer
-                device -> manufacturedBy -> manufacturer
-                sensor -> manufacturedBy -> manufacturer
-                device -> compatibleWith -> application
-                device -> compatibleWith -> device
-                device -> hasSensor -> sensor
-                application -> accessSensor -> sensor
-                process -> requiresSensor -> sensor
-                device -> performs -> process
-                application -> performs -> process
-                device -> hasPolicy -> privacyPolicy
-                application -> hasPolicy -> privacyPolicy
-                manufacturer -> hasPolicy -> privacyPolicy
-                process -> statesInPolicy -> privacyPolicy
-                sensor -> statesInPolicy -> privacyPolicy
-                observation -> statesInPolicy -> privacyPolicy
-                sensor -> captures -> observation
-                observation -> canInfer -> inference
-                inference -> canInfer -> inference
-                inference -> showsInference -> research
-                research -> references -> research
-                research -> hasTopic -> process
-                research -> hasTopic -> application
-                research -> hasTopic -> observation
-                research -> hasTopic -> sensor
-                research -> hasTopic -> device
-                privacyPolicy -> follows -> regulation
-                device -> isCategory -> category
-                ```
-                Note: This is a non-exhaustive list of the schema. Feel free to add more beyond the static schema as long as you follow the triplet format.
-
-                ### **Example Input:**
-                ```
-                {"entities": ["Govee", "Govee Smart Light Bulbs", "Alexa", "WiFi", "Google Privacy Policy", "General Data Protection Regulation (GDPR)"]}
-                ```
-
-                ### **Expected Output:**
-                ```
-                [
-                (('device', 'Govee Smart Light Bulbs'), 'manufacturedBy', ('manufacturer', 'Govee')),
-                (('device', 'Govee Smart Light Bulbs'), 'compatibleWith', ('application', 'Alexa')),
-                (('device', 'Govee Smart Light Bulbs'), 'isCategory', ('category', 'Smart Lighting')),
-                (('device', 'Govee Smart Light Bulbs'), 'hasSensor', ('sensor', 'WiFi')),
-                (('application', 'Alexa'), 'hasPolicy', ('privacyPolicy', 'Google Privacy Policy')),
-                (('privacyPolicy', 'Google Privacy Policy'), 'follows', ('regulation', 'General Data Protection Regulation (GDPR)'))
-                ]
-                ```
-
-                **Output must strictly follow this format with no additional text.** If no valid triplets exist, return:  
-                ```
-                []
-                ```
-
-                """,
-            },
-            {
-                'role': 'user',
-                'content': entities,
-            },
-        ],
-        stream=False
-    )
-
-    print(f"BEFORE PARSING <THINK> {response.message.content}")
+        # empties the cache
+        torch.cuda.empty_cache()
     
-    # removes think, json, and python tags
-    removed_think_tags = remove_think_tags(response.message.content)
-    return extract_python(extract_json(removed_think_tags))
+        # prompts deepseek to make triplets given a json on entities
+        response = client.chat(
+            model='deepseek-r1:70b', 
+            messages=[
+                {
+                    'role': 'system',
+                    'content':
+                    """
+                    Product Classification Task  
+Return only IOT or NOT-IOT. No explanation. No extra words. No punctuation.
+
+Examples:
+Amazon Echo Dot -> IOT  
+Apple Watch Strap -> NOT-IOT  
+Google Nest Thermostat -> IOT  
+Canon EOS Rebel Camera -> NOT-IOT  
+August Smart Lock Pro -> IOT  
+
+Now classify:
+{product_name} ->
+                    """,
+                },
+                {
+                    'role': 'user',
+                    'content': product_name,
+                },
+            ],
+            stream=False
+        )
+    
+        #print(f"BEFORE PARSING <THINK> {response.message.content}")
+        
+        # removes think, json, and python tags
+        removed_think_tags = remove_think_tags(response.message.content)
+    
+    
+        answer = removed_think_tags.upper()
+    
+        print(f"text_content {product_name}")
+        print(f"output {answer}")
+
+        if answer.strip()[-7:] == "NOT-IOT":
+            return False
+        elif answer.strip()[-3:] == "IOT":
+            return True
+    return False
+
+if __name__ == "__main__":
+
+    # initializes the parser
+    parser = argparse.ArgumentParser(description="Process an input file and save output.")
+    parser.add_argument("--input_file", required=True, help="json with configurations to a specific site")
+    parser.add_argument("--output_file", required=True, help="path to obtain the batch urls")
+
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    output_file = args.output_file
+
+    # opens the test file
+    with open(input_file, "r", encoding="utf-8", errors="replace") as f:
+        text_contents = f.readlines()
+
+    random.shuffle(text_contents)
+    text_contents = text_contents[:50]
+    with open(os.path.join(FOLDER, output_file), "w", encoding="utf-8") as f:
+        for content in tqdm(text_contents):
+
+            try:
+                # Step 1: Quote the URL if needed
+                content_fixed = re.sub(r'(https?://[^\s)]+)', r"'\1'", content.strip())
+    
+                # Step 2: Split into dict string and URL string
+                dict_str, url = content_fixed.rsplit(" ", 1)
+    
+                # Step 3: Safely evaluate the dictionary string
+                text_content = ast.literal_eval(dict_str)
+    
+                # Step 4: Classify product
+                result = product_classify(text_content)
+    
+                # Step 5: Write output
+                f.write(f"{text_content} | {url} | {result}\n")
+    
+            except Exception as e:
+                print(f"❌ Failed to parse line:\n{content}\nError: {e}\n")
+                continue
+            
