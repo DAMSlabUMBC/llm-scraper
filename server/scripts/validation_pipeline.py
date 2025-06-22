0452978be97d6d61f8c702b3b11abe683e8a1575
Gia Oriana Santos <gsantos2@chip.cm.cluster>
2025-06-22 09:09:22 -0400

    fixed validation pipeline

--- server/scripts/validation_pipeline.py
+++ server/scripts/validation_pipeline.py
@@ -2,16 +2,20 @@
 #from validation.llm_validation import llm_validation_method
 from search_validation_playwright import search_validation_method
 from llm_validation_playwright import llm_validation_method
+import ast
 
-def validation(triplet):
+def validation(triplet_str):
     weight = 0
+    
+    # converts the string triplet into its tuple form
+    triplet = ast.literal_eval(triplet_str)
 
     # simple method: search validation
     if weight < 50:
         try:
             weight = search_validation_method(triplet) or 0
         except Exception as e:
-            print(f"UNABLE TO PERFORM PERFORM SEARCH VALIDATION: {e}")
+            print(f"UNABLE TO PERFORM PERFORM SEARCH VALIDATION on {triplet}: {e}")
             weight = 0
     
     # medium method: validation with 1 llm
@@ -19,7 +23,7 @@ def validation(triplet):
         try:
             weight = llm_validation_method(triplet) or 0
         except Exception as e:
-            print(f"UNABLE TO PERFORM PERFORM LLM VALIDATION: {e}")
+            print(f"UNABLE TO PERFORM PERFORM LLM VALIDATION on {triplet}: {e}")
             weight = 0
 
     return weight
